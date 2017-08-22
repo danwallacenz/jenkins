d27437c72788fe3a3bf5f7bcbbdf310ca516d169
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-08-22 19:30:58 +1200

    remove wrong line

--- scripts/deploy-visualizer.sh
+++ scripts/deploy-visualizer.sh
@@ -4,12 +4,11 @@ docker stack deploy -c viz.yml visualizer
 
 while true; do
     VISUALIZER_REPLICA=$(docker service ls | grep visualizer_viz | awk '{print $4}')
-    VISUALIZER_REPLICA=$(docker service ls | grep proxy_swarm-listener | awk '{print $4}')
     if [[ $VISUALIZER_REPLICA == "1/1" ]]; then
         break
     else
         echo "Waiting for the Visualizer service..."
-        sleep 10
+        sleep 2
     fi
 done
 
